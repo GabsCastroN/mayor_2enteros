20697744f445286d899ab2a0dc58d7937f5de727
GabsCastroN <gabrielcastro@colegioguanenta.edu.co>
2023-02-22 07:39:24 -0500

    Se creo el programa py, se agrego el readme y el diagrama

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mayor_2enteros
 Programa para verificar cual de dos numeros enteros son mayor
+
+# Diagrama de flujo
+
+![Diagrama de flujo](diagrama.png "diagrama de flujo")
---
+++ mayor_enteros.py
@@ -0,0 +1,12 @@
+#input
+N = int (input("Dame un numero: "))
+P = int (input("Dame el otro numero: "))
+if N>P: 
+    X = N
+
+else: 
+    X = P 
+
+
+
+print("El entero mayor es: " + str(X))
